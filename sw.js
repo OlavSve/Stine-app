68de155ab355967ace1c6640e7ef38058581f602
OlavSve <olavsve@gmail.com>
2026-09-07 08:57:33 +0200

    Stine-app v3.21: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Kvitteringer, mjau mjau - bakgrunnsskript v3.20: nett først, hurtiglager som reserve (samme som NFK Operations 8.10) */
-const LAGER = "stine-app-v3-20";
+/* Kvitteringer, mjau mjau - bakgrunnsskript v3.21: nett først, hurtiglager som reserve (samme som NFK Operations 8.10) */
+const LAGER = "stine-app-v3-21";
 const GRUNN = ["./index.html", "./manifest.webmanifest", "./ikon-192.png", "./ikon-512.png"];
 
 self.addEventListener("install", h => {
